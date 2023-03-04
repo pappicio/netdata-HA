46c49e59c494ce9fa6f228dd592b975305f434ad
pappicio <52137123+pappicio@users.noreply.github.com>
2023-03-04 20:05:27 +0100

    Add files via upload

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -42,6 +42,10 @@ Partial Class Form1
         Me.Button4 = New System.Windows.Forms.Button()
         Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
         Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
         Me.ContextMenuStrip1.SuspendLayout()
         Me.ContextMenuStrip2.SuspendLayout()
         CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -76,7 +80,7 @@ Partial Class Form1
         'Button2
         '
         Me.Button2.Enabled = False
-        Me.Button2.Location = New System.Drawing.Point(16, 444)
+        Me.Button2.Location = New System.Drawing.Point(16, 516)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(354, 39)
         Me.Button2.TabIndex = 4
@@ -163,7 +167,7 @@ Partial Class Form1
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(12, 257)
+        Me.Label5.Location = New System.Drawing.Point(12, 340)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(153, 24)
         Me.Label5.TabIndex = 11
@@ -172,7 +176,7 @@ Partial Class Form1
         'Button3
         '
         Me.Button3.Enabled = False
-        Me.Button3.Location = New System.Drawing.Point(16, 294)
+        Me.Button3.Location = New System.Drawing.Point(16, 377)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(157, 39)
         Me.Button3.TabIndex = 12
@@ -182,7 +186,7 @@ Partial Class Form1
         'Button4
         '
         Me.Button4.Enabled = False
-        Me.Button4.Location = New System.Drawing.Point(213, 294)
+        Me.Button4.Location = New System.Drawing.Point(213, 377)
         Me.Button4.Name = "Button4"
         Me.Button4.Size = New System.Drawing.Size(157, 39)
         Me.Button4.TabIndex = 13
@@ -191,7 +195,7 @@ Partial Class Form1
         '
         'NumericUpDown1
         '
-        Me.NumericUpDown1.Location = New System.Drawing.Point(308, 393)
+        Me.NumericUpDown1.Location = New System.Drawing.Point(308, 465)
         Me.NumericUpDown1.Maximum = New Decimal(New Integer() {20, 0, 0, 0})
         Me.NumericUpDown1.Name = "NumericUpDown1"
         Me.NumericUpDown1.Size = New System.Drawing.Size(62, 28)
@@ -200,17 +204,59 @@ Partial Class Form1
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(12, 395)
+        Me.Label6.Location = New System.Drawing.Point(12, 467)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(284, 24)
         Me.Label6.TabIndex = 15
         Me.Label6.Text = "TABULATION SPACES (before):"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Enabled = False
+        Me.TextBox4.Location = New System.Drawing.Point(63, 272)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(260, 28)
+        Me.TextBox4.TabIndex = 16
+        Me.TextBox4.Text = "CPU"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(12, 245)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(163, 24)
+        Me.Label7.TabIndex = 17
+        Me.Label7.Text = "Search istances: 0"
+        '
+        'Button5
+        '
+        Me.Button5.Enabled = False
+        Me.Button5.Location = New System.Drawing.Point(329, 272)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(41, 28)
+        Me.Button5.TabIndex = 18
+        Me.Button5.Text = ">"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Enabled = False
+        Me.Button6.Location = New System.Drawing.Point(16, 272)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(41, 28)
+        Me.Button6.TabIndex = 19
+        Me.Button6.Text = "<"
+        Me.Button6.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 22.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1049, 734)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.TextBox4)
         Me.Controls.Add(Me.Label6)
         Me.Controls.Add(Me.NumericUpDown1)
         Me.Controls.Add(Me.Button4)
@@ -259,4 +305,8 @@ Partial Class Form1
     Friend WithEvents Button4 As Button
     Friend WithEvents NumericUpDown1 As NumericUpDown
     Friend WithEvents Label6 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
 End Class
